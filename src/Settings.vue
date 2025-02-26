dc6a51672515fa4b811069eb39c127720c7b222c
theia <theia@mit.edu>
2025-02-26 12:46:36 -0500

    settings buttons are disabled on load

--- src/Settings.vue
+++ src/Settings.vue
@@ -1,11 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const working = ref(false);
+</script>
+
 <template>
     <template v-if="$graffitiSession.value">
-        <button @click="$graffiti.logout($graffitiSession.value)">
-            Log out
+        <button
+            @click="
+                working = true;
+                $graffiti
+                    .logout($graffitiSession.value)
+                    .finally(() => (working = false));
+            "
+            :disabled="working"
+        >
+            {{ working ? "Logging out..." : "Log out" }}
         </button>
         <p>You are logged in as {{ $graffitiSession.value.actor }}</p>
     </template>
     <template v-else>
-        <button @click="$graffiti.login()">Log in</button>
+        <button
+            @click="
+                working = true;
+                $graffiti.login().finally(() => (working = false));
+            "
+            :disabled="working"
+        >
+            {{ working ? "Logging in..." : "Log in" }}
+        </button>
     </template>
 </template>
